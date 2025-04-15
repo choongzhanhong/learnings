78edf8cc818d189ad691be2c1e724f11911ab5e0
EGGYPLANT\zhanhong <choongorilla@gmail.com>
2025-04-15 22:01:21 +0800

    Add hittable and sphere

--- RayTracing/hittable.h
+++ RayTracing/hittable.h
@@ -8,12 +8,12 @@ class hit_record {
         point3 p;
         vec3 normal;
         double t;
-}
+};
 
 class hittable {
     public:
         virtual ~hittable() = default;
         
         virtual bool hit(const ray&r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
-}
+};
 #endif
